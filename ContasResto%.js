64e0d72780b933b2e2d0fba528cad8ee71697400
JordanFranck <161588431+JordanFranck@users.noreply.github.com>
2024-05-15 08:40:40 -0300

    Add files via upload

---
+++ ContasResto%.js
@@ -0,0 +1,19 @@
+var numero = prompt("digite um número");
+
+var resultado = numero %2
+
+  if(resultado == 0) {
+  alert("é par")
+  
+} else 
+    if(resultado == 1) {      
+      alert("é impar")
+      
+} else 
+    if(resultado == 1,2,3,4,5,6,7,8,9) {  
+      prompt("SÓ PODE NÚMEROS")
+
+} else 
+    if(resultado == null) {
+      alert("digite  número")
+}
